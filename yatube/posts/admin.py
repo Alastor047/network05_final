1d44164ceb6155e09bcad765d3805501063607b1
Andrey Kruglov <altos047@gmail.com>
2022-04-16 18:34:26 +0500

    final3

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from posts.models import Group, Post, Comment, Follow
+from posts.models import Comment, Follow, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
